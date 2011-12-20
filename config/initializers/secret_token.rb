7a8cd19b4c398b0bdb4855dd9c348223341ab86b
friermor <david@classwave.com>
2011-12-20 11:49:35 -0800

    David is awesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CWLandingpage::Application.config.secret_token = 'abef26e13c7f20a2387add55c775c3c904bbe6ca558e29fb5bb025bd364c9554fa44c45d26c8b55ac22990511ac1b443e3b7a91c73f36d74fc682c1517d9fd9c'
